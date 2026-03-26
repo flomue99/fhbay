05b7f9b78770dd087f8d8792dea8dcb16f36b97a
Your Name <florian.muehleder@gmail.com>
2026-03-26 11:05:37 +0100

    add rest

--- src/main/java/sve2/jee/rest/CustomerResource.java
+++ src/main/java/sve2/jee/rest/CustomerResource.java
@@ -1,4 +1,29 @@
 package sve2.jee.rest;
 
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import sve2.jee.domain.Customer;
+import sve2.jee.logic.CustomerAdmin;
+
+import java.util.List;
+
+@Path("customers")
 public class CustomerResource {
+
+    @Inject
+    CustomerAdmin customerAdmin;
+
+    @GET
+    @Path("")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Customer> getAllCustomers() {
+        List<Customer> customers = customerAdmin.findAllCustomers();
+        return customers;
+    }
+
+
+
 }
